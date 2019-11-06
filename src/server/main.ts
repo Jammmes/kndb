fe7e3c39f37f6149f3f1f5954aa77a1c42c9b493
Evgenii Vershinin <evgenii_vershinin@epam.com>
2019-11-06 14:25:40 +0400

    Init commit

---
+++ src/server/main.ts
@@ -0,0 +1,24 @@
+import * as express from 'express';
+import * as path from 'path';
+import { SERVER_PORT } from './config';
+import { apiRouter } from './routes/api-router';
+import { pagesRouter } from './routes/pages-router';
+import { staticsRouter } from './routes/statics-router';
+import * as config from './config';
+
+console.log(`*******************************************`);
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`config: ${JSON.stringify(config, null, 2)}`);
+console.log(`*******************************************`);
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static(path.join(process.cwd(), 'assets')));
+app.use(apiRouter());
+app.use(staticsRouter());
+app.use(pagesRouter());
+
+app.listen(SERVER_PORT, () => {
+  console.log(`App listening on port ${SERVER_PORT}!`);
+});

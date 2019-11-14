65314f4b6c172de6c9ce57195332356fb630e68a
Evgenii Vershinin <evgenii_vershinin@epam.com>
2019-11-14 11:46:17 +0400

    router: init router set

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Header, Content, Footer, Layout } from '@/components/layout';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
 import '@/styles/global.scss';
 
+import { Home } from '@/pages/home';
+import { Questions } from '@/pages/questions';
+
 class App extends React.Component<{}> {
   public render() {
     return (
-      <Layout>
-        <Header> Header</Header>
-        <Content> Content</Content>
-        <Footer>Footer</Footer>
-      </Layout>
+      <Router>
+        <Layout>
+          <Header> Header</Header>
+          <Content>
+            <Switch>
+              <Route path='/home' component = {Home} />
+              <Route path='/questions' component = {Questions} />
+              <Redirect to = '/home' />
+            </Switch>
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Router>
     );
   }
 }
